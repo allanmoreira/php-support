335dcaaa702ff4108210bb49d38cd6ffd3364052
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-07-30 12:33:36 -0300

    Mostrando dados na ordem de modalidade

--- js/paginas/calculadora_vendas.js
+++ js/paginas/calculadora_vendas.js
@@ -36,9 +36,10 @@ function consulta() {
         },
         success: function (response) {
             abreNotificacao('success', 'Consulta realizada com sucesso!');
+            mensagem.html(response.status.description);
             $('#tabela thead > tr').remove();
             $('#tabela tbody > tr').remove();
-            preencheTabela(response.data.tecnologias);
+            preencheTabela(response.data.tecnologias, organizaRetorno(response.data.tecnologias));
         },
         error: function (response) {
             if(response.status === 401){
@@ -48,10 +49,57 @@ function consulta() {
             }
         }
     });
-
 }
 
-function preencheTabela(tecnologias){
+function organizaRetorno(tecnologias){
+    var ordem = [
+        'Débito',
+        'Crediário',
+        'Crédito à vista',
+        'Parcelado 2x',
+        'Parcelado 3x',
+        'Parcelado 4x',
+        'Parcelado 5x',
+        'Parcelado 6x',
+        'Parcelado 7x',
+        'Parcelado 8x',
+        'Parcelado 9x',
+        'Parcelado 10x',
+        'Parcelado 11x',
+        'Parcelado 12x',
+        'Parcelado até 3x',
+        'Parcelado até 6x',
+        'Parcelado até 9x',
+        'Parcelado até 12x',
+        'Crédito Parcelado Emissor',
+    ];
+    var map_retorno = {};
+    $.each(ordem, function (i) {
+        map_retorno[ordem[i]] = [];
+    });
+    for (var key in map_retorno){
+        tecnologias.forEach(function (item_tecnologia){
+            var taxas = item_tecnologia.bandeiras[0].taxas;
+            for (let i = 0; i < taxas.length; i++) {
+                var taxa = taxas[i];
+                if(taxa.modalidade === key){
+                    map_retorno[key].push(taxa.valorReceber);
+                    break;
+                }
+            }
+        });
+    }
+    var retorno = [];
+    for (var key in map_retorno){
+        retorno.push({
+            modalidade : key,
+            taxas : map_retorno[key]
+        });
+    }
+    return retorno;
+}
+
+function preencheTabela(tecnologias, lista_taxas){
     var header = '<tr><th></th>';
     $.each(tecnologias, function (i) {
         header += '<th class="text-center">'+tecnologias[i].chaveTecnologia.toUpperCase()+'</th>';
@@ -60,17 +108,15 @@ function preencheTabela(tecnologias){
     $('#tabela thead').append(header);
 
     var html = '';
-    for (let j = 0; j < 14; j++) {
-        html += '<tr>';
-        var linha_adicionada = false;
-        $.each(tecnologias, function (i) {
-            if(!linha_adicionada){
-                html += '<td class="text-center" style="width: 10%"><strong>' + tecnologias[i].bandeiras[0].taxas[j].modalidade + '</strong></td>';
-                linha_adicionada = true;
-            }
-            html += '<td class="text-center">' + tecnologias[i].bandeiras[0].taxas[j].valorReceber + '</td>';
-        });
-        html += '<tr>';
-    }
+    lista_taxas.forEach(function (item_taxa_modalidade){
+        if(item_taxa_modalidade.taxas.length !== 0) {
+            html += '<tr>' +
+                '<td class="text-center" style="width: 200px"><strong>' + item_taxa_modalidade.modalidade + '</strong></td>';
+            item_taxa_modalidade.taxas.forEach(function (taxa) {
+                html += '<td class="text-center">' + taxa + '</td>';
+            });
+            html += '<tr>';
+        }
+    });
     $('#tabela tbody').append(html);
 }
