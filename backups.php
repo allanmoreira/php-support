966cd7d3709890093d1677649e53e8aea97eb495
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-25 01:52:22 -0300

    Listando arquivos

--- backups.php
+++ backups.php
@@ -1,8 +1,22 @@
 <?php require_once 'templates/header.php'; ?>
 
+    <div style="margin-left: 10px; margin-right: 10px; margin-top: 10px" class="row">
+        <h4><strong>Retorno</strong></h4>
+        <table id="tabela" class="table table-bordered">
+            <tbody>
+
 <?php
-    echo '<pre>' . var_export(scandir('/var/www/html/backups/planilha_financeira'), true) . '</pre>';
+    $arquivos = scandir('/var/www/html/backups/planilha_financeira');
+    foreach ($arquivos as $arquivo){
+        echo '<tr>' .
+                '<td>'. $arquivo .'</td>' .
+                '<td>'. date("F d Y H:i:s.", filemtime($arquivo)) . '</td>' .
+            '</tr>';
+    }
 ?>
+            </tbody>
+        </table>
+    </div>
 
 <?php require_once 'templates/scripts.php'; ?>
 <script src="static/js/jquery.mask.min.js"></script>
