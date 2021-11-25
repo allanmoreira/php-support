8d872bed322b9f41ac274e580d9d9bbb4ff70ce5
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-25 16:30:59 -0300

    Download

--- arquivos.php
+++ arquivos.php
@@ -17,7 +17,7 @@
     foreach ($arquivos as $arquivo){
         if($arquivo != '.' && $arquivo != '..')
         echo '<tr>' .
-                '<td><a href="download.php?diretorio='.$diretorio.'&arquivo='. $arquivo.'">'. $arquivo .'</a></td>' .
+                '<td><a href="download.php?tipo='.$tipo.'&projeto='. $projeto.'&arquivo='. $arquivo.'">'. $arquivo .'</a></td>' .
             '</tr>';
     }
 ?>
