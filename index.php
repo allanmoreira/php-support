57893fe5e9ff48c16356ed05baae67b2e2aaba70
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-25 01:01:56 -0300

    Inserindo PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once 'templates/header.php'; ?>
+
+    <div class="container theme-showcase" role="main">
+
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div class="jumbotron">
+            <h1>Template Testes</h1>
+            <p>Template de testes usando HTML e JS.</p>
+        </div>
+
+    </div> <!-- /container -->
+<?php require_once 'templates/scripts.php'; ?>
+<?php require_once 'templates/footer.php'; ?>
