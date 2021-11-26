a052cd3396925143aa23fac18522c25ec289da5c
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-26 13:08:28 -0300

    split retorno

--- backup.php
+++ backup.php
@@ -2,4 +2,7 @@
 
 require_once ('config/Config.php');
 
-echo shell_exec('sh ' . Config::getScriptBackup());
+$banco = $_GET['banco'];
+$saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco);
+$split = explode('-- ', $saida);
+echo json_encode($split);
