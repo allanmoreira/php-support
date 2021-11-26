7c68296a8042b32d7b3aba0bc039ccd102c40f7e
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-26 13:23:09 -0300

    get error

--- backup.php
+++ backup.php
@@ -3,7 +3,7 @@
 require_once ('config/Config.php');
 
 $banco = $_GET['banco'];
-$saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco);
+$saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco . ' 2>&1');
 $split = explode('-- ', $saida);
 foreach ($split as $linha){
     str_replace($linha, '\n', '');
