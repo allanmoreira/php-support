9d3ab371fca44eb12f54067f128fe9c3b7b42f25
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-26 13:13:23 -0300

    replace

--- backup.php
+++ backup.php
@@ -4,5 +4,8 @@ require_once ('config/Config.php');
 
 $banco = $_GET['banco'];
 $saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco);
-$split = explode('\n', $saida);
+$split = explode('-- ', $saida);
+foreach ($split as $linha){
+    str_replace($linha, '\\n', '');
+}
 echo json_encode($split);
