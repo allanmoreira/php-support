880945475ae0ab0387c5270967e72ca8787c0595
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-26 13:13:58 -0300

    replace

--- backup.php
+++ backup.php
@@ -6,6 +6,6 @@ $banco = $_GET['banco'];
 $saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco);
 $split = explode('-- ', $saida);
 foreach ($split as $linha){
-    str_replace($linha, '\\n', '');
+    str_replace($linha, '\n', '');
 }
 echo json_encode($split);
