4a1cf19c9a0bb037aa5ac5010ae3a3aec1d3696c
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-26 13:10:57 -0300

    split

--- backup.php
+++ backup.php
@@ -4,5 +4,5 @@ require_once ('config/Config.php');
 
 $banco = $_GET['banco'];
 $saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco);
-$split = explode('-- ', $saida);
+$split = explode('\n', $saida);
 echo json_encode($split);
