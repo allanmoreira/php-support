acb498f9bef380a724bc9a4d237aecd043602b48
Allan Moreira <allanfelipe.moreira@gmail.com>
2021-11-26 13:29:51 -0300

    backup direto no PHP

--- backup.php
+++ backup.php
@@ -3,9 +3,20 @@
 require_once ('config/Config.php');
 
 $banco = $_GET['banco'];
+
+$DB_NAME='planilha_financeira';
+$DB_PARAM='--clean --if-exists --create'; # --inserts'
+$DATE=`date +%Y-%m-%d-%H-%M-%S`;
+$BACKUP_DIR='/var/www/html/backups/$DATABASE';
+$BACKUP_NAME=$DATE.'-backup.sql';
+
+$saida = shell_exec('sh PGPASSWORD="mbrasilia0911" pg_dump -U postgres $DB_PARAM $DATABASE > $BACKUP_NAME');
+
+/*
 $saida = shell_exec('sh ' . Config::getScriptBackup() . ' ' . $banco . ' 2>&1');
 $split = explode('-- ', $saida);
 foreach ($split as $linha){
     str_replace($linha, '\n', '');
 }
 echo json_encode($split);
+*/
