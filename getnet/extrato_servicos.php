22a550e693928b9268d4e9a43d3a5455c5237974
Allan Moreira <allanfelipe.moreira@gmail.com>
2023-04-03 17:48:23 -0300

    Adicionando fontawesome

--- getnet/extrato_servicos.php
+++ getnet/extrato_servicos.php
@@ -10,6 +10,7 @@
         <!-- Bootstrap -->
         <link href="../static/css/bootstrap.min.css" rel="stylesheet">
         <link href="../static/css/layout.css" rel="stylesheet">
+        <link href="../static/css/plugins.css" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
